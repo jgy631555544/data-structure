5bcccff686a17852fab3643d9b45d0dfc1445229
guanyujin <jgy631555544@163.com>
2021-12-31 08:39:19 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/c.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/study/data-structure/linear_list/index.cpp" "CMakeFiles/c.dir/linear_list/index.cpp.obj" "gcc" "CMakeFiles/c.dir/linear_list/index.cpp.obj.d"
+  "C:/study/data-structure/main.cpp" "CMakeFiles/c.dir/main.cpp.obj" "gcc" "CMakeFiles/c.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/c.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c.dir/linear_list/index.cpp.obj"
+  "CMakeFiles/c.dir/linear_list/index.cpp.obj.d"
+  "CMakeFiles/c.dir/main.cpp.obj"
+  "CMakeFiles/c.dir/main.cpp.obj.d"
+  "c.exe"
+  "c.exe.manifest"
+  "c.pdb"
+  "libc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
