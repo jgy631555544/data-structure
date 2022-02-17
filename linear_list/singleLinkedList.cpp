8117cd9edac34f4f009c5f234cd77e457e57ee24
guanyujin <jgy631555544@163.com>
2022-02-17 21:50:50 +0800

    singleLinkedList add

--- linear_list/singleLinkedList.cpp
+++ linear_list/singleLinkedList.cpp
@@ -4,43 +4,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//不带头结点的链表
 typedef struct LNode {
     int data;
     struct LNode *next;
-} LNode, *Linklist;
+} LNode, *LinkList;
 
-bool InitList(Linklist &L);
+bool InitList(LinkList &L);
 
-bool Empty(Linklist L);
+bool Empty(LinkList L);
 
-bool ListInsert(Linklist &L, int i, int e);
+bool ListInsert(LinkList &L, int i, int e);
 
-bool InitList(Linklist &L) {
+bool InitList(LinkList &L) {
     L = NULL;
     return true;
 };
 
-bool Empty(Linklist L) {
+bool Empty(LinkList L) {
     return L == NULL;
 };
 
-bool ListInsert(Linklist &L, int i, int e) {
+bool ListInsert(LinkList &L, int i, int e) {
     if (i < 1) return false;
     if (i == 1) {
-        LNode *s = (LNode *) malloc(sizeof(LNode));
-        s->data = e;
-        s->next = L;
-        L = s;
+        LNode *node = (LNode *) malloc(sizeof(LNode));
+        node->data = e;
+        node->next = NULL;
+        L = node;
         return true;
+    }
+    LNode *p;
+    int index = 0;
+    p = L;
+    while (p!=NULL && index < i-1){
+        p = p->next;
+        index++;
     };
-    LNode *point;
-    int j = 0;
-
+    if(p == NULL) return false;
+    LNode *node = (LNode*) malloc(sizeof(LNode));
+    node->data = e;
+    node->next = p->next;
+    p->next = node;
     return true;
 }
 
+void TestPrint(bool test, char about[]) {
+    if (test)
+        printf("%s成功\n", about);
+    else
+        printf("%s失败\n", about);
+}
+
+void PrintList(LinkList L) {
+    //循环打印整个链表
+    LNode *p = L;//扫描指针
+    int j = 0;
+    if (p == NULL)printf("这是一个空表\n");
+    while (p != NULL) {
+        printf("LinkList[%d]=%d", j, p->data);
+        p = p->next;
+        j++;
+    }
+}
+
 int main() {
-    Linklist L;
-    InitList(L);
+    LinkList L;
+    TestPrint(InitList(L), "初始化");
+    PrintList(L);
+    TestPrint(ListInsert(L, 1, 2), "按位插入");
+    PrintList(L);
     return 0;
 };
--- linear_list/singleLinkedListHeadNode.cpp
+++ linear_list/singleLinkedListHeadNode.cpp
@@ -1,6 +1,97 @@
 //
 // Created by jgy66 on 2022/1/15.
 //
+#include <stdlib.h>
+
+typedef struct LNode {
+    int data;
+    struct LNode *next;
+} LNode, *LinkList;
+
+//函数声明
+bool InitList(LinkList &L);     //初始化
+bool Empty(LinkList L);         //盘空
+bool ListInsert(LinkList &L, int i, int e);//按位插入
+bool InsertNextNode(LNode *p, int e);//指定节点的后插操作
+bool InsertPriorNode(LNode *p, int e);//指定节点的前插操作————先完成后插，再交换数据以实现前插
+bool ListDelete(LinkList &L, int i, int &e);//按指定位序删除节点并返回其值
+bool DeleteNode(LNode *p);//删除指定节点
+
+bool InitList(LinkList &L) {
+    L = (LNode *) malloc(sizeof(LNode));
+    if (L == NULL) {
+        return false;
+    }
+    L->next = NULL;
+    return true;
+}
+
+bool Empty(LinkList L) {
+    return L->next == NULL;
+}
+
+bool ListInsert(LinkList &L, int i, int e) {
+    if (i < 0) return false;
+    LNode *p;
+    int index = 0;
+    p = L;
+    while (p != NULL && index < i - 1) {
+        p = p->next;
+        index++;
+    };
+    if (p == NULL) return false;
+    LNode *node = (LNode *) malloc(sizeof(LNode));
+    node->next = p->next;
+    node->data = e;
+    p->next = node;
+    return true;
+}
+
+bool InsertNextNode(LNode *p, int e) {
+    if (p == NULL)return false;
+    LNode *node = (LNode *) malloc(sizeof(LNode));
+    if (node == NULL)return false;
+    node->next = p->next;
+    node->data = e;
+    p->next = node;
+    return true;
+}
+
+bool InsertPriorNode(LNode *p, int e) {
+    if (p == NULL) return false;
+    LNode *node = (LNode *) malloc(sizeof(LNode));
+    if (node == NULL)return false;
+    node->next = p->next;
+    p->next = node;
+    node->data = p->data;
+    p->data = e;
+    return true;
+};
+
+bool ListDelete(LinkList &L, int i, int &e) {
+    if (i < 1)return false;
+    int index = 0;
+    LNode *node = L;
+    while (node->next != NULL && index < i - 1) {
+        node = node->next;
+        index++;
+    }
+    LNode *deleteNode = node->next;
+    e = deleteNode->data;
+    node->next = deleteNode->next;
+    free(deleteNode);
+    return true;
+}
+
+bool DeleteNode(LNode *p){
+    if(p == NULL) return false;
+    LNode *node = p->next;
+    p->data = node->data;
+    p->next = node->next;
+    free(node);
+    return true;
+};
+
 
 int main() {
     return 0;
